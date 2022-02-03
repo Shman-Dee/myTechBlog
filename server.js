91defdbd046e20edbec32ac651d99d347acb02ae
Andrew McInally <realfunnyestate@yahoo.com>
2022-02-03 12:31:14 -0700

    added a user model

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const express = require('express');
 
-const sequelize = require('./config/connection')
-
+const sequelize = require('./config');
+const routes = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log(`Now listening dad on port ${PORT}`));
+app.use(routes)
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening dad on port +${PORT}`));
+});
+
